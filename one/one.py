479640ffe9ab0621b87faf57f188dc45deeb7db8
Sam Duemler <samduemler@gmail.com>
2019-12-03 19:45:36 -0500

    Finished second day one puzzle.

--- one/one.py
+++ one/one.py
@@ -1,6 +1,17 @@
 input = open("input_one.txt", "r")
 lines = input.readlines()
+
+baseFuel = 0
 totalFuel = 0
+
 for x in lines:
-    totalFuel += ((int(x) // 3) - 2)
-print(totalFuel)
+    baseFuel += ((int(x) // 3) - 2)
+print(str(baseFuel) + " for all modules.")
+
+for x in lines:
+    mass = int(x)
+    while(mass >= 0):
+        mass = (mass // 3) - 2
+        if(mass > 0):
+            totalFuel += mass
+print(str(totalFuel) + " for all modules and additional fuel.")
